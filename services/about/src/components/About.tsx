c6fff23a9a86616c7dfc3be453a65020b6dfdaf3
denisberezin <dberezin@ippontech.ru>
2024-06-26 13:07:38 +0300

    fix content

--- services/about/src/components/About.tsx
+++ services/about/src/components/About.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            about page 1
+            <h3>Контент страницы About</h3>
         </div>
     );
 };
--- services/host/src/app/App.tsx
+++ services/host/src/app/App.tsx
@@ -1,31 +1,17 @@
 import React from 'react';
-import styles from './App.module.scss'
-import {Link, Outlet} from "react-router-dom";
-// @ts-ignore
-import Trash from 'about/About'
+
 import CurrentPage from "@packages/shared/src/components/CurrentPage";
-import Header from "@packages/shared/src/components/Header";
-import Footer from "@packages/shared/src/components/Footer";
+
 import Layout from "@packages/shared/src/components/Layout";
 
-// // @ts-ignore
-// import About from 'about/About'
 
 const App = () => {
-
-
-    //TODO: exposes: {
-    //         './App': './src/components/App'
-    //       },
-    // need exposes main component about
 
     return (
         <>
             <Layout>
                 <CurrentPage />
-                lalala
-                {/*<About />*/}
-                {/*<Outlet />*/}
+                <h3>Контент основной страницы</h3>
             </Layout>
         </>
     );
